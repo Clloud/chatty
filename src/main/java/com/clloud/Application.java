40ea9f15c37f7dea4935fa332787a777e4d2849c
Clloud <chenshaofengsf@163.com>
2020-02-23 14:21:30 +0800

    import idworker and utils

--- src/main/java/com/clloud/Application.java
+++ src/main/java/com/clloud/Application.java
@@ -11,7 +11,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 // 扫描mybatis mapper包路径
 @MapperScan(basePackages="com.clloud.mapper")
 // 扫描 所有需要的包, 包含一些自用的工具类包 所在的路径
-@ComponentScan(basePackages= {"com.clloud"})
+@ComponentScan(basePackages= {"com.clloud", "org.n3r.idworker"})
 public class Application {
 //	@Bean
 //	public SpringUtil getSpingUtil() {
